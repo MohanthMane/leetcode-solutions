8c4b95eb94ae2f3bef8ac0dedbcfdfde81ca5a49
Mohanth Mane <mohanth30@gmail.com>
2024-05-26 16:12:11 +0530

    Time: 17 ms (98.14%) | Memory: 46 MB (13.21%) - LeetSync

---
+++ 1186-building-h2o/building-h2o.java
@@ -0,0 +1,24 @@
+class H2O {
+
+    private final Semaphore hydrogenSemaphore;
+    private final Semaphore oxygenSemaphore;
+    
+    public H2O() {
+        this.hydrogenSemaphore = new Semaphore(2);
+        this.oxygenSemaphore = new Semaphore(0);
+    }
+
+    public void hydrogen(Runnable releaseHydrogen) throws InterruptedException {
+        hydrogenSemaphore.acquire();
+        // releaseHydrogen.run() outputs "H". Do not change or remove this line.
+        releaseHydrogen.run();
+        oxygenSemaphore.release();
+    }
+
+    public void oxygen(Runnable releaseOxygen) throws InterruptedException {
+        oxygenSemaphore.acquire(2);
+        // releaseOxygen.run() outputs "O". Do not change or remove this line.
+        releaseOxygen.run();
+        hydrogenSemaphore.release(2);
+    }
+}
