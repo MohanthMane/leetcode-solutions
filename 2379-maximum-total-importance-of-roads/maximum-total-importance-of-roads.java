a4ddfa29336b0745dadf7c0002ae266c9b04e9b0
Mohanth Mane <mohanth30@gmail.com>
2024-06-28 12:27:21 +0530

    Time: 6 ms (98.92%) | Memory: 62.3 MB (85.95%) - LeetSync

--- 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.java
+++ 2379-maximum-total-importance-of-roads/maximum-total-importance-of-roads.java
@@ -6,36 +6,13 @@ class Solution {
             degree[road[1]]++;
         }
 
-        PriorityQueue<Pair> queue = new PriorityQueue<>((a, b) -> b.y - a.y);
-        for (int i = 0; i < n; i++) {
-            queue.add(new Pair(i, degree[i]));
-        }
-
-        Map<Integer, Integer> nodeToImportance = new HashMap<>();
-        int importance = n;
-        while (!queue.isEmpty()) {
-            Pair pair = queue.poll();
-            nodeToImportance.put(pair.x, importance);
-            importance--;
-        }
-        
+        Arrays.sort(degree);
+        long value = 1;
         long result = 0;
-        for (int[] road : roads) {
-            result += nodeToImportance.get(road[0]) + nodeToImportance.get(road[1]);
+        for (int d: degree) {
+            result += (d * value);
+            value++;
         }
         return result;
-    }
-}
-
-class Pair {
-    int x, y;
-
-    public Pair(int x, int y) {
-        this.x = x;
-        this.y = y;
-    }
-
-    public Pair add(Pair toAdd) {
-        return new Pair(this.x + toAdd.x, this.y + toAdd.y);
     }
 }
