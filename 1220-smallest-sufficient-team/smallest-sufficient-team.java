15057621201aaa7a4b5a51c53fbcb5e12a738b34
Mohanth Mane <mohanth30@gmail.com>
2024-07-11 10:27:05 +0530

    Time: 6 ms (96.73%) | Memory: 42.6 MB (86.93%) - LeetSync

--- 1220-smallest-sufficient-team/smallest-sufficient-team.java
+++ 1220-smallest-sufficient-team/smallest-sufficient-team.java
@@ -1,38 +1,66 @@
+import java.util.*;
+
 class Solution {
     public int[] smallestSufficientTeam(String[] req_skills, List<List<String>> people) {
-        int n = people.size(), m = req_skills.length;
-        HashMap<String, Integer> skillId = new HashMap<String, Integer>();
-        for (int i = 0; i < m; i++) {
-            skillId.put(req_skills[i], i);
-        }
-        int skillsMaskOfPerson[] = new int[n];
+        int n = req_skills.length;
+        Map<String, Integer> skillsMap = new HashMap<>();
         for (int i = 0; i < n; i++) {
+            skillsMap.put(req_skills[i], i);
+        }
+
+        List<Set<Integer>> skillsPeopleTable = new ArrayList<>();
+        for (int i = 0; i < n; i++) {
+            skillsPeopleTable.add(new HashSet<>());
+        }
+
+        for (int i = 0; i < people.size(); i++) {
             for (String skill : people.get(i)) {
-                skillsMaskOfPerson[i] |= 1 << skillId.get(skill);
+                int skillIndex = skillsMap.get(skill);
+                skillsPeopleTable.get(skillIndex).add(i);
             }
         }
-        long dp[] = new long [1 << m];
-        Arrays.fill(dp, (1L << n) - 1);
-        dp[0] = 0;
-        for (int skillsMask = 1; skillsMask < (1 << m); skillsMask++) {
-            for (int i = 0; i < n; i++) {
-                int smallerSkillsMask = skillsMask & ~skillsMaskOfPerson[i];
-                if (smallerSkillsMask != skillsMask) {
-                    long peopleMask = dp[smallerSkillsMask] | (1L << i);
-                    if (Long.bitCount(peopleMask) < Long.bitCount(dp[skillsMask])) {
-                        dp[skillsMask] = peopleMask;
+
+        Queue<Pair> q = new LinkedList<>();
+        q.add(new Pair(skillsPeopleTable, new ArrayList<>()));
+
+        while (!q.isEmpty()) {
+            Pair curr = q.poll();
+            List<Set<Integer>> currTable = curr.table;
+            List<Integer> currTeam = curr.team;
+
+            Set<Integer> rareSkillPeopleSet = Collections.min(currTable, Comparator.comparingInt(Set::size));
+
+            for (int person : rareSkillPeopleSet) {
+                List<Set<Integer>> nextTable = new ArrayList<>();
+                for (Set<Integer> skillPeopleSet : currTable) {
+                    if (!skillPeopleSet.contains(person)) {
+                        nextTable.add(skillPeopleSet);
                     }
                 }
+                if (nextTable.isEmpty()) {
+                    int[] result = new int[currTeam.size() + 1];
+                    for (int i = 0; i < currTeam.size(); i++) {
+                        result[i] = currTeam.get(i);
+                    }
+                    result[currTeam.size()] = person;
+                    return result;
+                }
+                List<Integer> nextTeam = new ArrayList<>(currTeam);
+                nextTeam.add(person);
+                q.add(new Pair(nextTable, nextTeam));
             }
         }
-        long answerMask = dp[(1 << m) - 1];
-        int ans[] = new int [Long.bitCount(answerMask)];
-        int ptr = 0;
-        for (int i = 0; i < n; i++) {
-            if (((answerMask >> i) & 1) == 1) {
-                ans[ptr++] = i;
-            }
+
+        return new int[0]; // Should not reach here
+    }
+
+    class Pair {
+        List<Set<Integer>> table;
+        List<Integer> team;
+
+        Pair(List<Set<Integer>> table, List<Integer> team) {
+            this.table = table;
+            this.team = team;
         }
-        return ans;
     }
 }
