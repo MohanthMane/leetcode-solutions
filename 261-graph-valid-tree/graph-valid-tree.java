a2b3626cff823022a8c4d0e26a002c110ed35ad8
Mohanth Mane <mohanth30@gmail.com>
2024-07-16 16:31:39 +0530

    Time: 1 ms (79.09%) | Memory: 45.1 MB (26.27%) - LeetSync

---
+++ 261-graph-valid-tree/graph-valid-tree.java
@@ -0,0 +1,57 @@
+class DSU {
+
+    int[] parents;
+    int[] ranks;
+    int components;
+
+    public DSU(int size) {
+        this.components = size;
+        this.parents = new int[size];
+        this.ranks = new int[size];
+        for (int i = 0; i < size; i++) {
+            parents[i] = i;
+            ranks[i] = 1;
+        }
+    }
+
+    public int find(int x) {
+        if (parents[x] == x) {
+            return x;
+        }
+        return find(parents[x]);
+    }
+
+    public void union(int x, int y) {
+        int xParent = find(x);
+        int yParent = find(y);
+
+        if (xParent != yParent) {
+            if (ranks[xParent] > ranks[yParent]) {
+                parents[yParent] = xParent;
+                ranks[xParent] += ranks[yParent];
+            } else {
+                parents[xParent] = yParent;
+                ranks[yParent] += ranks[xParent];
+            }
+            this.components--;
+        }
+    }
+
+    public int getMaxRank() {
+        int max = 0;
+        for (int rank : ranks) {
+            max = Math.max(max, rank);
+        }
+        return max;
+    }
+}
+
+class Solution {
+    public boolean validTree(int n, int[][] edges) {
+        DSU dsu = new DSU(n);
+        for (int[] edge : edges) {
+            dsu.union(edge[0], edge[1]);
+        }
+        return dsu.components == 1 && edges.length == n - 1;
+    }
+}
