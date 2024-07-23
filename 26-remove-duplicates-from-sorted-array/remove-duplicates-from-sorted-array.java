67f5785e5743953dd8d86416ffdda03cd0540ceb
Mohanth Mane <mohanth30@gmail.com>
2024-07-23 17:01:20 +0530

    Time: 1 ms (78.47%) | Memory: 45 MB (10.32%) - LeetSync

--- 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
+++ 26-remove-duplicates-from-sorted-array/remove-duplicates-from-sorted-array.java
@@ -3,8 +3,7 @@ class Solution {
         int insertIndex = 1;
         for (int i = 1; i < nums.length; i++) {
             if (nums[i - 1] != nums[i]) {
-                nums[insertIndex] = nums[i];
-                insertIndex++;
+                nums[insertIndex++] = nums[i];
             }
         }
         return insertIndex;
