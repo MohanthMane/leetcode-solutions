2138964a63f5dbf3fac155e9eb5ca084fc4a2f40
Mohanth Mane <mohanth30@gmail.com>
2024-07-31 23:05:44 +0530

    Time: 12 ms (72.86%) | Memory: 54.3 MB (18.59%) - LeetSync

---
+++ 1002-maximum-width-ramp/maximum-width-ramp.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxWidthRamp(int[] nums) {
+        int n = nums.length;
+        Stack<Integer> stack = new Stack<>();
+        int ans = 0;
+
+        for (int i = 0; i < n; i++) {
+            if (stack.isEmpty() || nums[i] < nums[stack.peek()]) {
+                stack.push(i);
+            }
+        }
+
+        for (int i = n - 1; i > ans; i--) {
+            while (!stack.isEmpty() && nums[i] >= nums[stack.peek()]) {
+                ans = Math.max(ans, i - stack.pop());
+            }
+        }
+
+        return ans;
+    }
+}
