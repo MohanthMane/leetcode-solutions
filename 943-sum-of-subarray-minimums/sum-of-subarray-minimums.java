bdd0a0c56a26c5d2230fb072e33658869dc03169
Mohanth Mane <mohanth30@gmail.com>
2024-08-03 12:39:13 +0530

    Time: 67 ms (34.35%) | Memory: 52.2 MB (23.23%) - LeetSync

--- 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
+++ 943-sum-of-subarray-minimums/sum-of-subarray-minimums.java
@@ -2,28 +2,36 @@ class Solution {
 
     private static final int mod = 1000000007;
 
+    // OneNote
     public int sumSubarrayMins(int[] arr) {
         int n = arr.length;
         Stack<Integer> s = new Stack<>();
-        int[] dp = new int[n];
+        int[] prevSmaller = new int[n];
+        int[] nextSmaller = new int[n];
 
-        // dp[i] Sum of minimums for subarrays ending at i
         for (int i = 0; i < n; i++) {
-            while (!s.isEmpty() && arr[s.peek()] >= arr[i]) s.pop();
-
-            if (!s.isEmpty()) {
-                int previousSmallerIdx = s.peek();
-                dp[i] = dp[previousSmallerIdx] + (i - previousSmallerIdx) * arr[i];
-            } else {
-                dp[i] = (i + 1) * arr[i];
+            while (!s.isEmpty() && arr[s.peek()] > arr[i]) {
+                s.pop();
             }
+            prevSmaller[i] = s.isEmpty() ? -1 : s.peek();
             s.push(i);
         }
 
+        s.clear();
+        for (int i = n - 1; i >= 0; i--) {
+            while (!s.isEmpty() && arr[s.peek()] >= arr[i]) {
+                s.pop();
+            }
+            nextSmaller[i] = s.isEmpty() ? n : s.peek();
+            s.push(i);
+        }
         long result = 0;
-        for (int minimum: dp) {
-            result = result + minimum;
-            result %= mod;
+        for (int i = 0; i < n; i++) {
+            long leftInfluence = i - prevSmaller[i];
+            long rightInfluence = nextSmaller[i] - i;
+            long totalContribution = leftInfluence * rightInfluence;
+            long sum = totalContribution * arr[i];
+            result = (result + sum) % mod;
         }
         return (int) result;
     }
