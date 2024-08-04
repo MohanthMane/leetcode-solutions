945c742ed1f9be9f0cbc7aeb5b71f3120dd424e9
Mohanth Mane <mohanth30@gmail.com>
2024-08-04 20:32:42 +0530

    Time: 38 ms (33.85%) | Memory: 52.3 MB (31.69%) - LeetSync

---
+++ 1100-connecting-cities-with-minimum-cost/connecting-cities-with-minimum-cost.java
@@ -0,0 +1,47 @@
+class Solution {
+    public int minimumCost(int n, int[][] connections) {
+        DSU dsu = new DSU(n);
+        int cost = 0;
+        Arrays.sort(connections, Comparator.comparing(a -> a[2]));
+        for (int[] connection: connections) {
+            if (dsu.union(connection[0], connection[1])) {
+                cost += connection[2];
+            }
+        }
+        if (dsu.components != 1) return -1;
+        return cost;
+    }
+}
+
+class DSU {
+    int[] parents;
+    int components;
+
+    public DSU(int n) {
+        this.parents = new int[n + 1];
+        for (int i = 1; i <= n; i++) {
+            parents[i] = i;
+        }
+        this.components = n;
+    }
+
+    public int find(int x) {
+        if (parents[x] == x) return x;
+        return find(parents[x]);
+    }
+
+    public boolean union(int x, int y) {
+        int px = find(x);
+        int py = find(y);
+
+        if (px == py) return false;
+
+        if (px > py) {
+            parents[px] = py;
+        } else {
+            parents[py] = px;
+        }
+        this.components -= 1;
+        return true;
+    }
+}
