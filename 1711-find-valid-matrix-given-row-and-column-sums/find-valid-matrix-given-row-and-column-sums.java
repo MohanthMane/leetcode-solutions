f610d1f15b8ca7c9f6b0fe442a4df75545e3c53e
Mohanth Mane <mohanth30@gmail.com>
2024-08-14 12:04:54 +0530

    Time: 1 ms (100.00%) | Memory: 53 MB (34.31%) - LeetSync

--- 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.java
+++ 1711-find-valid-matrix-given-row-and-column-sums/find-valid-matrix-given-row-and-column-sums.java
@@ -1,21 +1,21 @@
 class Solution {
-
     public int[][] restoreMatrix(int[] rowSum, int[] colSum) {
         int n = rowSum.length, m = colSum.length;
         int[] currRowSum = new int[n];
         int[] currColSum = new int[m];
+        int row = 0, col = 0;
 
         int[][] result = new int[n][m];
-        int i = 0, j = 0;
-        while (i < n && j < m) {
-            result[i][j] = Math.min(rowSum[i] - currRowSum[i], colSum[j] - currColSum[j]);
-            currRowSum[i] += result[i][j];
-            currColSum[j] += result[i][j];
+        while (row < n && col < m) {
+            int toAdd = Math.min(rowSum[row] - currRowSum[row], colSum[col] - currColSum[col]);
+            currRowSum[row] += toAdd;
+            currColSum[col] += toAdd;
+            result[row][col] = toAdd;
 
-            if (currRowSum[i] == rowSum[i]) {
-                i++;
+            if (currRowSum[row] == rowSum[row]) {
+                row++;
             } else {
-                j++;
+                col++;
             }
         }
         return result;
