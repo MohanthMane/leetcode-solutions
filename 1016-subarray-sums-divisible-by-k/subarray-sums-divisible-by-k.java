fcb73305d46af809f16013067f301d4ad7799597
Mohanth Mane <mohanth30@gmail.com>
2024-08-21 12:31:48 +0530

    Time: 23 ms (62.27%) | Memory: 49.9 MB (6.88%) - LeetSync

--- 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
+++ 1016-subarray-sums-divisible-by-k/subarray-sums-divisible-by-k.java
@@ -1,15 +1,12 @@
 class Solution {
     public int subarraysDivByK(int[] nums, int k) {
-        int currentMod = 0;
-        int[] modGroups = new int[k];
-        modGroups[0] = 1;
-        for (int num : nums) {
-            currentMod = (((currentMod + num) % k) + k) % k;
-            modGroups[currentMod]++;
-        }
-        int result = 0;
-        for (int modGroup : modGroups) {
-            result += modGroup * (modGroup - 1) / 2;
+        Map<Integer, Integer> m = new HashMap<>();
+        m.put(0, 1);
+        int sum = 0, result = 0;
+        for (int num: nums) {
+            sum = (((sum + num) % k) + k) % k;
+            result += m.getOrDefault(sum, 0);
+            m.put(sum, m.getOrDefault(sum, 0) + 1);
         }
         return result;
     }
