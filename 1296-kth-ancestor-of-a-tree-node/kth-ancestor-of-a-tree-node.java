8a827b2c225a5579da20b685f7e038803a4e87fb
Mohanth Mane <mohanth30@gmail.com>
2024-08-21 11:30:41 +0530

    Time: 67 ms (59.65%) | Memory: 87.9 MB (44.44%) - LeetSync

--- 1296-kth-ancestor-of-a-tree-node/kth-ancestor-of-a-tree-node.java
+++ 1296-kth-ancestor-of-a-tree-node/kth-ancestor-of-a-tree-node.java
@@ -1,26 +1,34 @@
+// Errichto's video - https://www.youtube.com/watch?v=oib-XsjFa-M
 class TreeAncestor {
 
     /*
-        dp state: dp[i][j] = j-th node's 2^(i)th ancestor in the path 
-        dp init: dp[i][j] = dp[0][j] (first parent (2^0) of each node is given)
-        Formula : dp[i][j] = dp[i-1][dp[i-1][j]]
+        dp state: dp[i][j] = i-th node's 2^(j)th ancestor in the path 
+        dp init: dp[i][j] = dp[i][0] (first parent (2^0) of each node is given)
+        Formula : dp[i][j] = dp[dp[i][j - 1]][j - 1]
+
+        To compute kth ancestor, take an example of 19.
+        Binary representation will have 16, 2, 1
+        If we know the 16th parent, 2nd parent and 1st parent of every node,
+        we can compute the ancestor in log n time
     */
     private int[][] dp;
     private int maxDepth;
 
     public TreeAncestor(int n, int[] parent) {
         this.maxDepth = (int) (Math.log(n) / Math.log(2)) + 1;
-        this.dp = new int[maxDepth][n];
-        dp[0] = parent;
+        this.dp = new int[n][maxDepth];
+        
+        for (int i = 0; i < parent.length; i++) {
+            dp[i][0] = parent[i];
+        }
 
-        for (int i = 1; i < maxDepth; i++) {
-            for (int node = 0; node < n; node++) {
-                int preAncestor = dp[i - 1][node];
-                // No more ancestors
-                if (preAncestor == -1) {
-                    dp[i][node] = -1;
+        for (int j = 1; j < maxDepth; j++) {
+            for (int i = 0; i < n; i++) {
+                int prevAncestor = dp[i][j - 1];
+                if (prevAncestor == -1) {
+                    dp[i][j] = -1;
                 } else {
-                    dp[i][node] = dp[i - 1][preAncestor];
+                    dp[i][j] = dp[dp[i][j - 1]][j - 1];
                 }
             }
         }
@@ -30,7 +38,7 @@ class TreeAncestor {
         int currNode = node;
         for (int i = 0; i < maxDepth; i++) {
             if (((k >> i) & 1) == 1) {
-                currNode = dp[i][currNode];
+                currNode = dp[currNode][i];
             }
             if (currNode == -1) return -1;
         }
