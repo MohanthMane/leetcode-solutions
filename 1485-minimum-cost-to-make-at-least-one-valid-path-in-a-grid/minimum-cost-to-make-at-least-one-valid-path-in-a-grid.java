6d2eb470acd859aaba4e5b1597e649115d338288
Mohanth Mane <mohanth30@gmail.com>
2024-08-22 14:38:31 +0530

    Time: 12 ms (92.05%) | Memory: 45.2 MB (35.23%) - LeetSync

--- 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
+++ 1485-minimum-cost-to-make-at-least-one-valid-path-in-a-grid/minimum-cost-to-make-at-least-one-valid-path-in-a-grid.java
@@ -1,35 +1,39 @@
 class Solution {
-    
-    private int INF = (int) 1e9;
-    private int[][] DIR = new int[][] { { 0, 1 }, { 0, -1 }, { 1, 0 }, { -1, 0 } };
+
+    private static final int[][] directions = new int[][] {{0, 1}, {0, -1}, {1, 0}, {-1, 0}};
 
     public int minCost(int[][] grid) {
-        int m = grid.length, n = grid[0].length, cost = 0;
-        int[][] dp = new int[m][n];
+        int m = grid.length, n = grid[0].length;
+        int[][] changes = new int[m][n];
         for (int i = 0; i < m; i++)
-            Arrays.fill(dp[i], INF);
-        Queue<int[]> bfs = new LinkedList<>();
-        dfs(grid, 0, 0, dp, cost, bfs);
-        while (!bfs.isEmpty()) {
-            cost++;
-            for (int size = bfs.size(); size > 0; size--) {
-                int[] top = bfs.poll();
-                int r = top[0], c = top[1];
-                for (int i = 0; i < 4; i++)
-                    dfs(grid, r + DIR[i][0], c + DIR[i][1], dp, cost, bfs);
+            Arrays.fill(changes[i], Integer.MAX_VALUE);
+
+        Deque<int[]> queue = new ArrayDeque<>();
+        queue.add(new int[] {0, 0, 0});
+
+        while (!queue.isEmpty()) {
+            int[] current = queue.pollFirst();
+            int x = current[0], y = current[1], change = current[2];
+            if (x == m - 1 && y == n - 1) return change;
+
+            for (int i = 0; i < directions.length; i++) {
+                int[] direction = directions[i];
+                int newX = x + direction[0], newY = y + direction[1];
+                if (newX >= m || newY >= n || newX < 0 || newY < 0) continue;
+
+                if ((i + 1) == grid[x][y]) {
+                    if (change < changes[newX][newY]) {
+                        queue.addFirst(new int[] {newX, newY, change});
+                        changes[newX][newY] = change;
+                    }
+                } else {
+                    if (change + 1 < changes[newX][newY]) {
+                        queue.addLast(new int[] {newX, newY, change + 1});
+                        changes[newX][newY] = change + 1;
+                    }
+                }
             }
         }
-        return dp[m - 1][n - 1];
-    }
-
-    void dfs(int[][] grid, int r, int c, int[][] dp, int cost, Queue<int[]> bfs) {
-        int m = grid.length;
-        int n = grid[0].length;
-        if (r < 0 || r >= m || c < 0 || c >= n || dp[r][c] != INF)
-            return;
-        dp[r][c] = cost;
-        bfs.offer(new int[] { r, c });
-        int nextDir = grid[r][c] - 1;
-        dfs(grid, r + DIR[nextDir][0], c + DIR[nextDir][1], dp, cost, bfs);
+        return changes[m - 1][n - 1];
     }
 }
