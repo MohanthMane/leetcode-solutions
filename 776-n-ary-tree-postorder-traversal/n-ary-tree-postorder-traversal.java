635e0a6b1991d7fbf876b4538e2dbccd27cd0840
Mohanth Mane <mohanth30@gmail.com>
2024-08-26 19:48:31 +0530

    Time: 1 ms (34.54%) | Memory: 44.4 MB (83.57%) - LeetSync

---
+++ 776-n-ary-tree-postorder-traversal/n-ary-tree-postorder-traversal.java
@@ -0,0 +1,39 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> children;
+
+    public Node() {}
+
+    public Node(int _val) {
+        val = _val;
+    }
+
+    public Node(int _val, List<Node> _children) {
+        val = _val;
+        children = _children;
+    }
+};
+*/
+
+class Solution {
+
+    private List<Integer> result = new ArrayList<>();
+
+    public List<Integer> postorder(Node root) {
+        dfs(root);
+        return result;
+    }
+
+    private void dfs(Node root) {
+        if (root != null) {
+            if (root.children != null) {
+                for (Node child: root.children) {
+                    dfs(child);
+                } 
+            }
+            result.add(root.val);
+        }
+    } 
+}
