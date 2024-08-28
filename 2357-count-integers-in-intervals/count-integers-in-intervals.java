b2543f157da13f4c27eb6cd5497b9d12f7f322dc
Mohanth Mane <mohanth30@gmail.com>
2024-08-28 11:16:37 +0530

    Time: 80 ms (80.95%) | Memory: 97.4 MB (34.13%) - LeetSync

--- 2357-count-integers-in-intervals/count-integers-in-intervals.java
+++ 2357-count-integers-in-intervals/count-integers-in-intervals.java
@@ -1,43 +1,49 @@
 class CountIntervals {
     
-    // Interval treemap start -> finish.
-    private final TreeMap<Integer, Integer> s;
-    private int count;
+    TreeSet<Interval> intervals;
+    int count;
+    
     public CountIntervals() {
-        this.s = new TreeMap<Integer, Integer>();
-        this.count = 0;
+        intervals = new TreeSet<>(new Comparator<Interval>() {
+           public int compare(Interval left, Interval right) {
+               if (left.right == right.right) {
+                   return left.left - right.left;
+               }
+               return left.right - right.right;
+           } 
+        });
     }
-
+    
     public void add(int left, int right) {
-        Map.Entry<Integer, Integer> floorEntry = s.floorEntry(right);
-        if (floorEntry == null || floorEntry.getValue() < left) {
-            s.put(left, right);
-            count += (right - left + 1);
-        } else {
-            int start = left;
-            int end = right;
-
-            // Remove overlapping intervals and update count.
-            while (true) {
-                int l = s.floorKey(end);
-                int r = s.get(l);
-                start = Math.min(start, l);
-                end = Math.max(end, r);
-                count -= (r - l + 1);
-                s.remove(l);
-                // Break the loop until there is no overlapping with interval (start, end).
-                floorEntry = s.floorEntry(end);
-                if (floorEntry == null || floorEntry.getValue() < start) {
-                    break;
-                }
+        Interval searchInterval = new Interval(0, left);
+        int start = left;
+        int end = right;
+        Iterator<Interval> itr = intervals.tailSet(searchInterval).iterator();
+        while (itr.hasNext()) {
+            Interval current = itr.next();
+            if (left <= current.right && right >= current.left) {
+                start = Math.min(start, current.left);
+                end = Math.max(end, current.right);
+                count -= current.right - current.left + 1;
+                itr.remove();
+            } else {
+                break;
             }
-            // Add (start, end) to TreeMap and update count.
-            s.put(start, end);
-            count += (end - start + 1);
         }
+        intervals.add(new Interval(start, end));
+        count += end - start + 1;
     }
-
-    public int count() {
+    
+    public int count() {      
         return count;
+    }
+}
+
+class Interval {
+    int left, right;
+
+    public Interval(int l, int r) {
+        this.left = l;
+        this.right = r;
     }
 }
