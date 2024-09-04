4e44e30e9b72e8f900419b86aa8131ca40e3c212
Mohanth Mane <mohanth30@gmail.com>
2024-09-04 15:36:34 +0530

    Time: 524 ms (31.73%) | Memory: 46.4 MB (41.52%) - LeetSync

--- 3563-select-cells-in-grid-with-maximum-score/select-cells-in-grid-with-maximum-score.py
+++ 3563-select-cells-in-grid-with-maximum-score/select-cells-in-grid-with-maximum-score.py
@@ -1,30 +1,26 @@
 class Solution:
-    def maxScore(self, grid):
-        rows, cols = len(grid), len(grid[0])
-        unique = set(grid[r][c] for r in range(rows) for c in range(cols))
-        sorted_unique = sorted(list(unique), reverse = True)
-        val_to_rows = defaultdict(list)
-        
-        for r in range(rows):
-            for c in range(cols):
-                val = grid[r][c]
-                if r not in val_to_rows[val]:
-                    val_to_rows[val].append(r)
+    def maxScore(self, grid: List[List[int]]) -> int:
+        n = len(grid)
+        d = defaultdict(list)
+        for i in range(n):
+            for val in grid[i]:
+                d[val].append(i)
+        vals = list(d.items())
+        cache = defaultdict(int)
 
-        def dfs(chosen, values, score, idx):
-            if idx == len(values):
-                return score
+        def solve(i, mask):
+            if i >= len(vals):
+                return 0
             
-            scores = []
-            for row in val_to_rows[values[idx]]:
-                if row not in chosen:
-                    chosen.add(row)
-                    scores.append(dfs(chosen, values, score + values[idx], idx + 1))
-                    chosen.remove(row)
-            
-            if len(scores) == 0:
-                scores.append(dfs(chosen, values, score, idx + 1))
+            key = str(i) + "," + str(mask)
+            if key in cache.keys():
+                return cache[key]
 
-            return max(scores)
-        
-        return dfs(set(), sorted_unique, 0, 0)
+            res = solve(i + 1, mask)
+            for j in vals[i][1]:
+                if not mask & (1 << j):
+                    res = max(res, vals[i][0] + solve(i + 1, mask | (1 << j)))
+            cache[key] = res
+            return res
+
+        return solve(0, 0)
