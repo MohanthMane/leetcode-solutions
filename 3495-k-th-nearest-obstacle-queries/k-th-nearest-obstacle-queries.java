da54c3d3f7a61c18b0a21d3ded3fceaac38f764b
Mohanth Mane <mohanth30@gmail.com>
2024-09-04 15:06:34 +0530

    Time: 245 ms (5.22%) | Memory: 169.2 MB (9.72%) - LeetSync

--- 3495-k-th-nearest-obstacle-queries/k-th-nearest-obstacle-queries.java
+++ 3495-k-th-nearest-obstacle-queries/k-th-nearest-obstacle-queries.java
@@ -2,8 +2,8 @@ class Solution {
     public int[] resultsArray(int[][] queries, int k) {
         PriorityQueue<Integer> queue = new PriorityQueue<>(Comparator.comparing(a -> -a));
         int[] result = new int[queries.length];
-        int idx = 0;
-        for (int[] query: queries) {
+        for (int i = 0; i < queries.length; i++) {
+            int[] query = queries[i];
             int dist = Math.abs(query[0]) + Math.abs(query[1]);
             if (queue.size() < k) {
                 queue.offer(dist);
@@ -13,11 +13,10 @@ class Solution {
             }
 
             if (queue.size() == k) {
-                result[idx] = queue.peek();
+                result[i] = queue.peek();
             } else {
-                result[idx] = -1;
+                result[i] = -1;
             }
-            idx++;
         }
         return result;
     }
